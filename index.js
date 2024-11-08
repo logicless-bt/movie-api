63505c2351dd6048391ce7719f6c9f4f6bc5b459
logicless-bt <benpturner01@gmail.com>
2024-11-07 20:15:35 -0500

    FavoriteMovies fixed, Delete
    -made FavoriteMovies reference Movie rather than the nonexistant Movies
    -changed findOneAndRemove to findOneAndDelete

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ app.put('/users/:Username', async (req, res) => {
 
 //DELETE
 app.delete('/users/:Username', async (req, res) => {
-  await Users.findOneAndRemove({ Username: req.params.Username })
+  await Users.findOneAndDelete({ Username: req.params.Username })
     .then((user) => {
       if (!user) {
         res.status(400).send(req.params.Username + ' was not found');
