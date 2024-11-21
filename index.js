73eb1103dacf559e2a42c3bd5df7a137b329bc77
logicless-bt <benpturner01@gmail.com>
2024-11-20 22:40:57 -0500

    fixed env variable
    -changed env variable from string to variable

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 //mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //setting up morgan, static, body-parser, auth, and passport
 app.use(morgan('common'));
